7a49e3d6e14d24f69a80d4360c9dee68c519a0dc
Anri <67234362+anritsetskhla10@users.noreply.github.com>
2024-05-21 21:18:29 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+// let string = "hello"
+
+// let str = new String("hello");
+
+// console.log(str.length)
+
+
+// let num = null;
+// console.log(num)
+
+// let isTrue = Boolean(true)
+
+// console.log( typeof num)
+
+// let sym = Symbol('tses')
+// let sym2 = Symbol('tses')
+
+// console.log(sym === sym2)
+
+// let symbol = Symbol('key');
+
+// let obj = {
+//     [symbol]: "value"
+// }
+
+// console.log(JSON.stringify(obj))
+
+// let person = new Object()
+
+// let calculator = {
+//     add: function(a,b){
+//         return a + b
+//     }
+// }
+
+// console.log(calculator.add( 3 , 6))
+
+
+// for ( let key in person){
+//     console.log(key + ":" + person[key])
+// }
+
+
+// let { name, age, city} = person
+
+// let array = new Array(7);
+
